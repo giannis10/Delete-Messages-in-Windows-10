dde2c7b12bdc6e623678f26b5ba2bd2998f2d8e1
giannis10 <80069359+giannis10@users.noreply.github.com>
2023-12-08 09:05:28 +0200

    Create Delete-Messages-in-Windows-10.bat

---
+++ Delete-Messages-in-Windows-10.bat
@@ -0,0 +1,4 @@
+@echo off
+del C:\Users\{username}\AppData\Local\Microsoft\Windows\INetCache\Low\Content.IE5\* /q
+del C:\Users\{username}\AppData\Local\Microsoft\Windows\INetCache\* /q
+del C:\Users\{username}\AppData\Local\Temp\* /q
